1e37ee1fc5d2a45f814322a42628d76961f175f6
Miri <mirivhbv@gmail.com>
2020-02-09 20:43:25 +0400

    Adds Binary Tree

---
+++ DataStructures/BinaryTree/BinaryTree.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    public class BinaryTree<T> : IEnumerable<T>
+        where T : IComparable<T>
+    {
+        private BinaryTreeNode<T> _head;
+        private int _count;
+
+        #region Add
+
+        public void Add(T value)
+        {
+            // Case 1: The tree is empty - allocate the head
+            if (_head == null)
+            {
+                _head = new BinaryTreeNode<T>(value);
+            }
+            // Case 2: The tree is not empty so find the right location to insert
+            else
+            {
+                AddTo(_head, value);
+            }
+
+            _count++;
+        }
+
+        // Recursive add algorithm
+        private void AddTo(BinaryTreeNode<T> node, T value)
+        {
+            // Case 1: Value is less than the current node value
+            if (value.CompareTo(node.Value) < 0)
+            {
+                // if there is no left child make this the new left
+                if (node.Left == null)
+                {
+                    node.Left = new BinaryTreeNode<T>(value);
+                }
+                // else add it to the left node
+                else
+                {
+                    AddTo(node.Left, value);
+                }
+            }
+            // Case 2: Value is equal to or greater than the current value
+            else
+            {
+                // if there is no right, add it to the right
+                if (node.Right == null)
+                {
+                    node.Right = new BinaryTreeNode<T>(value);
+                }
+                // else add it to the right node
+                else
+                {
+                    AddTo(node.Right, value);
+                }
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Determines if the specified value exists in the binary tree.
+        /// </summary>
+        /// <param name="value">The value to search for</param>
+        /// <returns>True if the tree contains the value, false otherwise</returns>
+        public bool Contains(T value)
+        {
+            // defer to the node search helper function.
+            BinaryTreeNode<T> parent;
+            return FindWithParent(value, out parent) != null;
+        }
+
+        /// <summary>
+        /// Finds and returns the first node containing the specified value.
+        /// If the value is not found, returns null. Alse returns the parent of 
+        /// the node (or null) which is used in Remove.
+        /// </summary>
+        /// <param name="value">The value to search for</param>
+        /// <param name="parent">The parent of the found node (or null)</param>
+        /// <returns>The found node (or null)</returns>
+        private BinaryTreeNode<T> FindWithParent(T value, out BinaryTreeNode<T> parent)
+        {
+            // Now, try to find data in the tree
+            var current = _head;
+            parent = null;
+
+            // while we do not have a match
+            while (current != null)
+            {
+                int result = current.CompareTo(value);
+
+                if (result > 0)
+                {
+                    // if the value is less than current, go left
+                    parent = current;
+                    current = current.Left;
+                }
+                else if (result < 0)
+                {
+                    // if the value is more than current, go right.
+                }
+                else
+                {
+                    // we have a match
+                    break;
+                }
+            }
+
+            return current;
+        }
+
+        #region Remove
+
+        /// <summary>
+        /// Removes the first occurance of the speciied value from the tree.
+        /// </summary>
+        /// <param name="value">The value to remove</param>
+        /// <returns>True if the value was removed, false otherwise</returns>
+        public bool Remove(T value)
+        {
+            BinaryTreeNode<T> current, parent;
+
+            current = FindWithParent(value, out parent);
+        
+            if (current == null)
+            {
+                return false;
+            }
+
+            // Case 1: If current has no right child, then current's left replaces current
+            if (current.Right == null)
+            {
+                if (parent == null)
+                {
+                    _head = current.Left;
+                }
+                else
+                {
+                    int result = parent.CompareTo(current.Value);
+                    if (result > 0)
+                    {
+                        // if parent value is greater than current value
+                        // make the current left child a left child of parent
+                        parent.Left = current.Left;
+                    }
+                    else if (result < 0)
+                    {
+                        // if parent value is less than current value
+                        // make the current left child a right child of parent
+                        parent.Right = current.Left;
+                    }
+                }
+            }
+            // Case 2: If current's right child has no left child, then current's right child
+            //         replaces current
+            else if (current.Right.Left == null)
+            {
+                current.Right.Left = current.Left;
+
+                if (parent == null)
+                {
+                    _head = current.Right;
+                }
+                else
+                {
+                    int result = parent.CompareTo(current.Value);
+
+                    if (result > 0)
+                    {
+                        parent.Left = current.Right;
+                    }
+                    else if (result < 0)
+                    {
+                        parent.Right = current.Right;
+                    }
+                }
+            }
+            // Case 3: If the current's right child has a left child, replace current with current's
+            //         right child's left-most child
+            else
+            {
+                // Find the right's left-most child
+                var leftmost = current.Right.Left;
+                var leftmostParent = current.Right;
+
+                while (leftmost.Left != null)
+                {
+                    leftmostParent = leftmost;
+                    leftmost = leftmost.Left;
+                }
+
+                // the parent's  left subtree becomes the leftmost's right subtree
+                leftmostParent.Left = leftmost.Right;
+
+                // assign leftmost's left and right to current's left and right children
+                leftmost.Left = current.Left;
+                leftmost.Right = current.Right;
+
+                // <<<<<
+            }
+        }
+
+
+        #endregion
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
